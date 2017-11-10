3187669e08e60081be53788f3939549847825f63
eboy <giannisduke@gmail.com>
2017-11-10 20:41:50 +0200

    remove test

--- site/web/app/themes/carhub_v1/templates/unit-gravityform.php
+++ site/web/app/themes/carhub_v1/templates/unit-gravityform.php
@@ -3,9 +3,6 @@
     <div class="row">
       <div class="col-12 text-center feedback">
         <span class="underline_arrow"> Your Booking </span>
-        <div id="feedback">
-          test
-        </div>
 <?php gravity_form(1, false, false, false, '', true, 12); ?>
 
       <button class="btn btn-primary btn-lg" id="submit">Get Quote!</button>
